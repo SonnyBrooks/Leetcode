9db523c1c4750200c7fc321a2595eae803d306ef
Andrew Budziszek <andrew@budziszek.com>
2022-02-04 12:33:43 -0600

    added 167 using ts

---
+++ leetcode167.ts
@@ -0,0 +1,24 @@
+/**
+ * Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+ */
+
+// Really slow
+
+function twoSum(numbers: number[], target: number): number[] | void {
+    for(var i = 0; i < numbers.length; i++) {
+        if(numbers[i] + numbers[numbers.length - 1] < target) continue; //Skip all numbers smaller than your target
+        for(var k = i + 1; k < numbers.length; k++) {
+            if(numbers[i] + numbers[k] === target) return [i + 1, k + 1];
+            if(numbers[i] + numbers[k] > target) break;
+        }
+    }
+};
+
+var numbers = [2,7,11,15];
+console.log(twoSum(numbers, 9))
