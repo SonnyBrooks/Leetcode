1cd1eee6c8b5c6bb5c9dc1bc5a18c848d481d348
Andrew Budziszek <andrew@budziszek.com>
2022-02-09 12:56:18 -0600

    adding 1920

---
+++ leetcode1920.js
@@ -0,0 +1,20 @@
+/**
+ * Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+ * A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var buildArray = function(nums) {
+    let arr = [];
+    for(let i = 0; i < nums.length; i++) {
+        arr[i] = nums[nums[i]];
+    }
+
+    return arr;
+};
+
+let e1 = [0,2,1,5,3,4]
+console.log(buildArray(e1))
